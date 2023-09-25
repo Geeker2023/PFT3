666d48e39ab28819111097260099d1968d2c4f0b
Geeker2023 <geek2023er@gmail.com>
2023-09-25 12:34:28 +0300

    Package for project created

---
+++ sandbox/src/main/java/en/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,15 @@
+package en.pft.sandbox;
+
+public class MyFirstProgram {
+  public static void main(String[] args) {
+    String somebody = "world";
+    System.out.println("Hello, " + somebody + "!");
+
+    double l = 8;
+    double s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+
+  }
+
+
+}
